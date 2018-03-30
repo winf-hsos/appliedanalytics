6790beae8cce8d8b4909bbe93b2f9171c9492bb8
Nicolas Meseth <n.meseth@hs-osnabrueck.de>
2018-03-30 22:03:50 +0000

    Added notebook template for phase 1

---
+++ databricks-notebooks/fallstudie/fallstudie-phase-1-template.scala
@@ -0,0 +1,242 @@
+// Databricks notebook source
+// MAGIC %md
+// MAGIC # Infos zum Template
+// MAGIC Dies ist eine Vorlage für die Phase 1 der Fallstudie. Diese Vorlage ist zwingend zu verwenden, zumindest für die finale Abgabe der Lösungen.
+// MAGIC 
+// MAGIC In der ersten Phase wählt ihr aus 3 Custern Aufgaben mit verschiedenen Komplexitätspunkten (= maximale erreichbare Punkte) aus. In Summe müsst ihr mit den ausgewählten Aufgaben mindestens 40 (mögliche) Komplexitätspunkte erreichen. In dieser Vorlage sind alle Aufgaben enthalten. Aufgaben die ihr nicht bearbeitet, könnt ihr einfach aus dem Template entfernen. Alternativ gebt ihr einfach im Kommentar an, dass ihr diese Aufgabe nicht bearbeitet. **Jede Aufgabe, für die ein Lösungsansatz abgegeben wird, wird auch bewertet**.
+// MAGIC 
+// MAGIC Unter jeder Frage ist in dieser Vorlage jeweils 1 Codeblock eingefügt. Das bedeutet nicht, dass ihr nur einen Codeblock verwenden dürft. Fügt so viele darunter ein, wie ihr benötigt. Stellt aber sicher, dass erkennbar ist zu welcher Aufgabe ein Codeblock gehört, indem er als Nachfolger des Blocks mit der Überschrift erscheint.
+// MAGIC 
+// MAGIC Sollet ihr Visualisierungen verwenden, so lasst diese in der von euch erstellten Form als Ergebnis im Notebook bestehen, so dass sie mit exportiert werden. Ein Neuausführen kann dazu führen, dass Formatierungen oder Einstellungen der Grafik verändert werden.
+// MAGIC 
+// MAGIC Bitte beachtet sämtliche Informationen im <a href="https://docs.google.com/document/d/1PBpaL1v7__gg1LQkPJ9iKQD10cFU0yVwU6rKW3FK_Ko/edit?usp=sharing" target="_blank">Dokument zum Ablauf und Bewertung der Fallstudie</a>.
+
+// COMMAND ----------
+
+// MAGIC %md
+// MAGIC # Fallstudie - Phase 1
+
+// COMMAND ----------
+
+// MAGIC %md
+// MAGIC ## Cluster 1 - Sortiment
+
+// COMMAND ----------
+
+// MAGIC %md
+// MAGIC ### 1.1: Wie viele unterschiedliche Marken bietet REWE im Online-Shop an?
+// MAGIC ⬤
+
+// COMMAND ----------
+
+
+
+// COMMAND ----------
+
+// MAGIC %md
+// MAGIC ### 1.2: Erstellt eine Liste der Biermarken, sortiert nach der Anzahl der Produkte im Sortiment!
+// MAGIC ⬤ ⬤
+
+// COMMAND ----------
+
+
+
+// COMMAND ----------
+
+// MAGIC %md
+// MAGIC ### 1.3: Gemessen an der Anzahl der Produkte, wie hoch ist der Non-Food Anteil im gesamten Sortiment?
+// MAGIC ⬤ ⬤ ⬤
+
+// COMMAND ----------
+
+
+
+// COMMAND ----------
+
+// MAGIC %md
+// MAGIC ### 1.4: Wie hoch ist der Bioanteil in jeder Produktkategorie?
+// MAGIC ⬤ ⬤ ⬤
+
+// COMMAND ----------
+
+
+
+// COMMAND ----------
+
+// MAGIC %md
+// MAGIC ### 1.5: Aus wie vielen Wörtern bestehen die Produktbeschreibungen im Schnitt?
+// MAGIC ⬤ ⬤ ⬤
+
+// COMMAND ----------
+
+ 
+
+// COMMAND ----------
+
+// MAGIC %md
+// MAGIC ### 1.6: Welche Adjektive werden in den Produktbeschreibungen am häufigsten verwendet?
+// MAGIC ⬤ ⬤ ⬤ ⬤ ⬤
+
+// COMMAND ----------
+
+
+
+// COMMAND ----------
+
+// MAGIC %md
+// MAGIC ### 1.7: Welche Nudelhersteller sind im Sortiment vertreten und mit welchem Anteil? Zeigt nur solche mit einem Anteil von 5% oder mehr und weist alle anderen als “Sonstige” aus!
+// MAGIC ⬤ ⬤ ⬤ ⬤ ⬤ &nbsp;&nbsp; ⬤ ⬤ ⬤
+
+// COMMAND ----------
+
+
+
+// COMMAND ----------
+
+// MAGIC %md
+// MAGIC ## Cluster 1 - Inhaltsstoffe
+
+// COMMAND ----------
+
+// MAGIC %md
+// MAGIC ### 2.1: Welche Produkte sind bio **und** vegan?
+// MAGIC ⬤
+
+// COMMAND ----------
+
+
+
+// COMMAND ----------
+
+// MAGIC %md
+// MAGIC ### 2.2: Welche Produkte - neben reinem Salz - enthalten das meiste Salz pro 100g?
+// MAGIC ⬤ ⬤
+
+// COMMAND ----------
+
+
+
+// COMMAND ----------
+
+// MAGIC %md
+// MAGIC ### 2.3: Welcher Anteil aller Lebensmittelprodukte enthält Gluten?
+// MAGIC ⬤ ⬤
+
+// COMMAND ----------
+
+
+
+// COMMAND ----------
+
+// MAGIC %md
+// MAGIC ### 2.4: Wie viele Produkte enthalten eine Form von Zucker als Zutat?
+// MAGIC ⬤ ⬤ ⬤
+
+// COMMAND ----------
+
+
+
+// COMMAND ----------
+
+// MAGIC %md
+// MAGIC ### 2.5: Welche Biere im Sortiment sind nach dem Deutschen Reinheitsgebot gebraut?
+// MAGIC ⬤ ⬤ ⬤ ⬤ ⬤
+
+// COMMAND ----------
+
+
+
+// COMMAND ----------
+
+// MAGIC %md
+// MAGIC ### 2.6: Welche 10 Zutaten kommen über alle Produkte hinweg am häufigsten vor?
+// MAGIC ⬤ ⬤ ⬤ ⬤ ⬤
+
+// COMMAND ----------
+
+
+
+// COMMAND ----------
+
+// MAGIC %md
+// MAGIC ### 2.7: Erstellt eine Liste aus der hervorgeht, wie oft 2 Inhaltsstoffe zusammen in einem Produkt vorkommen!
+// MAGIC ⬤ ⬤ ⬤ ⬤ ⬤ &nbsp;&nbsp;  ⬤ ⬤ ⬤
+
+// COMMAND ----------
+
+
+
+// COMMAND ----------
+
+// MAGIC %md
+// MAGIC ## Cluster 1 - Preise
+
+// COMMAND ----------
+
+// MAGIC %md
+// MAGIC ### 3.1: Welche Produktkategorie hat das höchste Preisniveau?
+// MAGIC ⬤
+
+// COMMAND ----------
+
+
+
+// COMMAND ----------
+
+// MAGIC %md
+// MAGIC ### 3.2: Welches sind die 10 teuersten Rotweine im Sortiment?
+// MAGIC ⬤ ⬤
+
+// COMMAND ----------
+
+
+
+// COMMAND ----------
+
+// MAGIC %md
+// MAGIC ### 3.3: Erstellt eine Liste der Windeln, sortiert nach Preis pro Stück!
+// MAGIC ⬤ ⬤ ⬤
+
+// COMMAND ----------
+
+
+
+// COMMAND ----------
+
+// MAGIC %md
+// MAGIC ### 3.4: Vergleicht die Preise der REWE Eigenmarken mit denen der Premiummarken für Tafelschokolade (100 g)!2.2: Welche Produkte - neben reinem Salz - enthalten das meiste Salz pro 100g?
+// MAGIC ⬤ ⬤ ⬤ ⬤ ⬤
+
+// COMMAND ----------
+
+
+
+// COMMAND ----------
+
+// MAGIC %md
+// MAGIC ### 3.5: Erstellt einen Bericht, der die Produkte in die Preisklassen “weniger 1 EUR”, “1 bis 2 EUR”, “2 - 10 EUR” und “mehr als 10 EUR” einordnet? Wie ist die Verteilung nach Anzahl der Produkte in jeder Klasse?
+// MAGIC ⬤ ⬤ ⬤ ⬤ ⬤
+
+// COMMAND ----------
+
+
+
+// COMMAND ----------
+
+// MAGIC %md
+// MAGIC ### 3.6: Wie ist das Preisverhältnis zwischen Bioprodukten und herkömmlichen Produkten aus der Kategorie “Obst und Gemüse”?
+// MAGIC ⬤ ⬤ ⬤ ⬤ ⬤
+
+// COMMAND ----------
+
+
+
+// COMMAND ----------
+
+// MAGIC %md
+// MAGIC ### 3.7: Erstellt eine Liste mit Getränken, aufsteigend sortiert nach dem Preis pro 100 kcal!
+// MAGIC ⬤ ⬤ ⬤ ⬤ ⬤ &nbsp;&nbsp; ⬤ ⬤ ⬤
+
+// COMMAND ----------
+
+
