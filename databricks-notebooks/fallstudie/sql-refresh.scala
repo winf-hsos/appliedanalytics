4506b3a521485e1798277c1a7ca1b3da37310bfb
Nicolas Meseth <n.meseth@hs-osnabrueck.de>
2018-04-03 21:54:30 +0000

    Added SQL refresh notebook

---
+++ databricks-notebooks/fallstudie/sql-refresh.scala
@@ -0,0 +1,54 @@
+// Databricks notebook source
+// MAGIC %md
+// MAGIC # SQL Refresh
+// MAGIC Die folgenden Aufgaben sollen euren SQL Kenntnissen (wieder) auf die Sprünge helfen!
+
+// COMMAND ----------
+
+// MAGIC %md
+// MAGIC ### 1. Welches Produkt hat die GTIN 4000521006549?
+
+// COMMAND ----------
+
+// MAGIC %md
+// MAGIC ### 2: Wie viele Produkte sind im Sortiment enthalten?
+
+// COMMAND ----------
+
+// MAGIC %md 
+// MAGIC ### 3. Welches Obst hat die meisten Proteine?
+
+// COMMAND ----------
+
+// MAGIC %md
+// MAGIC ### 4. Welche drei Kategorien enthalten die meisten Produkte?
+
+// COMMAND ----------
+
+// MAGIC %md
+// MAGIC ### 5. Wie ist die prozentuale Verteilung der Produkte auf die Kategorien?
+
+// COMMAND ----------
+
+// MAGIC %md 
+// MAGIC ### 6. Welches ist das zweitteuerste Craft Bier im Sortiment?
+
+// COMMAND ----------
+
+// MAGIC %md
+// MAGIC ### 7. Welche Produkte sind laut Beschreibung exklusiv?
+
+// COMMAND ----------
+
+// MAGIC %md
+// MAGIC ### 8. Erstellt eine Liste aller Whiskeys sortiert nach dem Preis (teuerster oben) und dem relativen Preis im Vergleich zum teuersten im % als Spalte!
+
+// COMMAND ----------
+
+// MAGIC %md
+// MAGIC ### 9: Erstellt eine Liste der Biermarken, sortiert nach der Anzahl der Produkte im Sortiment!
+
+// COMMAND ----------
+
+// MAGIC %md
+// MAGIC ###  10: Wie viele Produkte enthalten eine Form von Zucker als Zutat?
